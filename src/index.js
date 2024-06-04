bfa905d9fdb623bf07b4634817dc765aa1872a11
khadija <itstirm@gmail.com>
2024-06-04 08:30:12 -0700

    Final changes, webpack

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import './components/Error.js';
+import './components/JobList.js';
+import './components/Search.js';
+import './components/Spinner.js';
+import './components/Sorting.js';
+import './components/Pagination.js';
+import './components/Bookmarks.js';
+import './components/Storage.js';
+import './components/Router.js';
+
+import './index.css';
